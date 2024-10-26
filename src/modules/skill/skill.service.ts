99ccd081abe67e7013aad136c43d29fb0fa4d6ea
Bui Van Do <87635349+bvdo23@users.noreply.github.com>
2024-10-26 15:48:08 +0700

    feat: apis working skill

--- src/modules/skill/skill.service.ts
+++ src/modules/skill/skill.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { BaseService } from '@/utils/base/base.service';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from '@/prisma/prisma.service';
+import { AllSkillArgs } from './skill.type';
+import { SkillWhereInput } from '@/prisma/graphql';
+import { responseHelper } from '@/utils/helpers';
 
 @Injectable()
 export class SkillService implements BaseService {
@@ -15,8 +18,24 @@ export class SkillService implements BaseService {
   findFirst(args: Prisma.SkillFindFirstArgs) {
     return this.prismaService.skill.findFirst(args);
   }
-  findMany(args: Prisma.SkillFindManyArgs) {
-    return this.prismaService.skill.findMany(args);
+  async findMany(args: AllSkillArgs) {
+    const { searchValue, pagination, where, ...reset } = args;
+    let whereClause: SkillWhereInput = {};
+    if (searchValue && searchValue.length > 0) {
+      whereClause.OR = [{ content: { contains: searchValue, mode: 'insensitive' } }];
+    }
+    if (where) {
+      whereClause = {
+        AND: [whereClause, where]
+      };
+    }
+    const data = this.prismaService.skill.findMany({
+      where: whereClause,
+      orderBy: { createdAt: 'desc' },
+      ...reset
+    });
+    const total = await this.count({ where: whereClause });
+    return responseHelper(data, { total, ...pagination });
   }
   count(args: Prisma.SkillCountArgs) {
     return this.prismaService.skill.count(args);
